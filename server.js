fdef77c4e8d55500d203171fc00783eae7b38f23
hoangduylq <lqhoangduy@gmail.com>
2021-10-21 12:45:56 +0700

    fix condition for production in server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
